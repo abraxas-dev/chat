341d9542b0c8950161117b198b510d5e1cb8ff85
Abraxas <abraxas.w@proton.me>
2025-05-06 17:56:48 +0200

    added main page

--- app.py
+++ app.py
@@ -16,13 +16,29 @@ def init_session():
         st.session_state.session_id = str(uuid.uuid4())
     if "messages" not in st.session_state:
         st.session_state.messages = []
-    if "graph" not in st.session_state:
-        st.session_state.graph, config = create_graph(st.session_state.session_id)
+    # if "graph" not in st.session_state:
+    #     st.session_state.graph, config = create_graph(st.session_state.session_id)
 
 def welcome_page():
-    if st.button("Let's Go!"):
+    st.markdown("""
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+                """, unsafe_allow_html=True)
+    
+    st.markdown("""
+        <div class="flex flex-col items-center justify-center min-h-[70vh] px-4 mt-14">
+        <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-12 text-gray-800">
+            Welcome to your AI Assistant
+        </h1>
+        <h4 class="text-lg md:text-xl lg:text-2xl text-gray-600 text-center mb-12 max-w-2xl">
+            I'm your AI assistant. Ask me anything!
+        </p>
+        </div>
+    """, unsafe_allow_html=True)
+
+    col1, col2, col3 = st.columns([1, 2, 1])
+    if col2.button("Let's Chat!", key="welcome_button", use_container_width=True):
         st.session_state.started = True
-        st.experimental_rerun()
+        st.rerun()
 
 def chat_page():
     pass
